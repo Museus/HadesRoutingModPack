fbdb30fb6a41ef66e1f2f3bdf5e65fc20140bf38
Museus <museus@protonmail.com>
2020-11-28 01:59:21 -0500

    ShowChamberNumber: renamed from ChamberNumberMod

--- ShowChamberNumber/ShowChamberNumber.lua
+++ ShowChamberNumber/ShowChamberNumber.lua
@@ -1,26 +1,26 @@
 --[[
-    ChamberNumberMod
+    ShowChamberNumber
     Authors:
       Museus (Discord: Museus#7777)
       Early Access patch by ShipGoSync (Discord: Ship#0101)
       Overriding HideDepthCounter idea by Ellomenop (Discord: ellomenop#2254)
 
-    This version of the ChamberNumberMod shows the Depth immediately upon starting a room,
+    This version of ShowChamberNumber shows the Depth immediately upon starting a room,
     rather than waiting for the CombatUI to spawn. If that fails for some reason, fall back
     to showing the Depth during ShowCombatUI.
 ]]
-ModUtil.RegisterMod("ChamberNumberMod")
+ModUtil.RegisterMod("ShowChamberNumber")
 
 ModUtil.WrapBaseFunction("StartRoom", function ( baseFunc, currentRun, currentRoom )
     ShowDepthCounter()
     baseFunc(currentRun, currentRoom)
-end, ChamberNumberMod)
+end, ShowChamberNumber)
 
 ModUtil.WrapBaseFunction("ShowCombatUI", function ( baseFunc, flag )
     ShowDepthCounter()
     baseFunc(flag)
-end, ChamberNumberMod)
+end, ShowChamberNumber)
 
 -- Hiding Depth Counter doesn't actually do anything
 ModUtil.BaseOverride("HideDepthCounter", function ( baseFunc )
-end, ChamberNumberMod)
+end, ShowChamberNumber)
